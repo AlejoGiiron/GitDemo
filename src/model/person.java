c36fcf1f1fda17afb1c7ce569ece16cf17515ffb
AlejoGiiron <invitado@s1pc32.eisc.univalle.edu.co>
2023-03-30 15:25:30 -0500

    Create Professor Class

--- src/model/person.java
+++ src/model/person.java
@@ -9,5 +9,29 @@ package model;
  * @author invitado
  */
 public class person {
+    public int id;
+    public String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    
     
 }
